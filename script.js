c8581be20cb79843068c94ac2c21277fc6569f8a
Potassium3 <miles.plowman@gmail.com>
2025-12-05 13:50:20 +0000

    save

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const width = window.innerWidth;
+const height = window.innerHeight;
+
 function quicksin(x) {
     return Math.sin(x*(3.14159*2)/360)
 }
@@ -25,16 +28,14 @@ function convert3D2D(x, y, z, rot, ele) {
     return projected
 }
 
-function drawPolygon(arr) {
-    const width = 1000
-    const height = 1000
+function drawPolygon(arr, rgb) {
     let game = document.getElementById("game");
     let clipPath = ""
     for (let item of arr) {
         clipPath += `calc(${item[0]+width/2}px) calc(${item[1]+height/2}px),`
     }
     clipPath = clipPath.slice(0, -1);
-    game.innerHTML += `<div style="clip-path: polygon(${clipPath})"></div>`
+    game.innerHTML += `<div style="clip-path: polygon(${clipPath}); background-color: rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]});"></div>`
 }
 
 function clear() {
@@ -42,12 +43,10 @@ function clear() {
     game.innerHTML = "";
 }
 
-function showBlock(rot, ele, x, y, z, xw, yw, zw) {
+function showBlock(rot, ele, x, y, z, xw, yw, zw, rgb) {
     let xr = (rot%360) < 180
     let yr = (rot%360) < 90 || (rot%360) >= 270
     let zr = false
-    console.log(rot, ele)
-    console.log(xr, yr, zr);
     // Draw x face
     if (!xr) {
         drawPolygon([
@@ -55,14 +54,14 @@ function showBlock(rot, ele, x, y, z, xw, yw, zw) {
             convert3D2D(x, y+yw, z, rot, ele),
             convert3D2D(x, y+yw, z+zw, rot, ele),
             convert3D2D(x, y, z+zw, rot, ele),
-        ]);
+        ], [rgb[0], rgb[1], rgb[2]]);
     } else {
         drawPolygon([
             convert3D2D(x+xw, y, z, rot, ele),
             convert3D2D(x+xw, y+yw, z, rot, ele),
             convert3D2D(x+xw, y+yw, z+zw, rot, ele),
             convert3D2D(x+xw, y, z+zw, rot, ele),
-        ]);
+        ], [rgb[0], rgb[1], rgb[2]]);
     }
     // Draw y face
     if (!yr) {
@@ -71,14 +70,14 @@ function showBlock(rot, ele, x, y, z, xw, yw, zw) {
             convert3D2D(x+xw, y, z, rot, ele),
             convert3D2D(x+xw, y, z+zw, rot, ele),
             convert3D2D(x, y, z+zw, rot, ele),
-        ]);
+        ], [rgb[0]-15, rgb[1]-15, rgb[2]-15]);
     } else {
         drawPolygon([
             convert3D2D(x, y+yw, z, rot, ele),
             convert3D2D(x+xw, y+yw, z, rot, ele),
             convert3D2D(x+xw, y+yw, z+zw, rot, ele),
             convert3D2D(x, y+yw, z+zw, rot, ele),
-        ]);
+        ], [rgb[0]-15, rgb[1]-15, rgb[2]-15]);
     }
     // Draw z face
     if (!zr) {
@@ -87,20 +86,97 @@ function showBlock(rot, ele, x, y, z, xw, yw, zw) {
             convert3D2D(x+xw, y, z, rot, ele),
             convert3D2D(x+xw, y+yw, z, rot, ele),
             convert3D2D(x, y+yw, z, rot, ele),
-        ]);
+        ], [rgb[0]-8, rgb[1]-8, rgb[2]-8]);
     } else {
         drawPolygon([
             convert3D2D(x, y, z+zw, rot, ele),
             convert3D2D(x+xw, y, z+zw, rot, ele),
             convert3D2D(x+xw, y+yw, z+zw, rot, ele),
             convert3D2D(x, y+yw, z+zw, rot, ele),
-        ]);
+        ], [rgb[0]-8, rgb[1]-8, rgb[2]-8]);
     }
 }
 
-let rot = 4;
+function interpretLML(data, x, y, z, xw, yw, zw) {
+    let xr = (rot%360) < 180
+    let yr = (rot%360) < 90 || (rot%360) >= 270
+    let zr = false
+    if (!data.includes("(")) {
+        if (data == "green") {
+            showBlock(30, 60, x, y, z, xw, yw, zw, [127, 255, 127]);
+
+        } else if (data == "blue") {
+            showBlock(30, 60, x, y, z, xw, yw, zw, [255, 255, 127]);
+
+        }
+        return
+    }
+    let split = data[0]
+
+    let bracketdepth = 0;
+    let dimension = "";
+    let text = "";
+    let nexttext = "";
+    let next = false;
+    let bracketalready = false;
+    for (let char of data) {
+        if (char == "(") {
+            bracketdepth++;
+            bracketalready = true;
+        } else if (char == ")") {
+            bracketdepth--;
+        }
+        if (bracketdepth == 0 && bracketalready) {
+            next = true;
+        }
+        if (bracketdepth > 0 && char != "(") {
+            if (!next) {
+                text += char;
+            } else {
+                nexttext += char;
+            }
+        } else if (char != "(" && char != ")") {
+            dimension += char;
+        }
+    }
+
+    dimension = parseFloat(dimension.substring(1));
+
+    if (split === "x") {
+        if (xr) {
+            interpretLML(text, x, y, z, dimension, yw, zw);
+            interpretLML(nexttext, x+dimension, y, z, xw-dimension, yw, zw);
+        } else {
+            interpretLML(nexttext, x+dimension, y, z, xw-dimension, yw, zw);
+            interpretLML(text, x, y, z, dimension, yw, zw);
+        }
+    } else if (split === "y") {
+        if (yr) {
+            interpretLML(text, x, y, z, xw, yw-dimension, zw);
+            interpretLML(nexttext, x, y+dimension, z, xw, dimension, zw);
+        } else {
+            interpretLML(nexttext, x, y, z, xw, yw, zw);
+            interpretLML(text, x, y, z, xw, yw, zw);
+        }
+    } else if (split === "z") {
+        interpretLML(text, x, y, z, dimension, yw, zw);
+        interpretLML(nexttext, x, y, z+dimension, xw, yw, zw-dimension);
+    }
+}
+
+const WORLD = `
+x(
+green
+)
+30
+(
+blue
+)
+`.replaceAll("\n", "").replaceAll(" ", "")
+
+let rot = 30
+let ele = 45
 setInterval(function() {
     clear();
-    showBlock(rot, 45, 0, 0, 0, 20, 100, 300);
-    rot += 2;
+    interpretLML(WORLD, -250, -250, -250, 500, 500, 500);
 }, 50)
